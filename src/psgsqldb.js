6bd1c40f6f9633670a539e9499f1bf94fc111427
KevinZambran <kevinzambrano593@gmail.com>
2023-07-08 17:17:06 -0500

    conexion database

--- src/psgsqldb.js
+++ src/psgsqldb.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 // Configuración de conexión a la base de datos
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'user',
-    password: '567',
+    user: 'rvsuxgiu',
+    host: 'stampy.db.elephantsql.com',
+    database: 'rvsuxgiu',
+    password: '2lobdShcYwLzXfIJQGS5kho4rjuTKBC3',
     port: 5432,
   });
 
